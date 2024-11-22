c6348dde5cfedcbdeb2733599968fb07b737c5a3
Elliot Mayer <elliot.w.mayer@gmail.com>
2024-11-21 23:27:42 -0800

    binary searches

--- src/iterativeBinarySearch.py
+++ src/iterativeBinarySearch.py
@@ -1,15 +1,15 @@
-def binarySearch(arr, x):
+def binarySearch(list, value):
     low = 0
-    high = len(arr) - 1
+    high = len(list) - 1
+    mid = high // 2
 
     while low <= high:
         mid = (low + high) // 2
-        if arr[mid] > x:
+        if value < list[mid]:
             high = mid - 1
-        elif arr[mid] < x:
-            low = mid + 1 
+        elif value > list[mid]:
+            low = mid + 1
         else:
             return mid
-    return -1
 
 print(binarySearch([1,2,3,4,5,6,7,8,9,10], 6))
--- src/recursiveBinarySearch.py
+++ src/recursiveBinarySearch.py
@@ -1,14 +1,11 @@
-def binarySearch(arr, low, high, x):
+def binarySearch(list, low, high, value):
     mid = (low + high) // 2
     if low <= high:
-        if arr[mid] > x:
-            return binarySearch(arr, low, mid - 1, x)
-        if arr[mid] < x:
-            return binarySearch(arr, mid + 1, high, x)
+        if value < list[mid]:
+            return binarySearch(list, low, mid - 1, value)
+        elif value > list[mid]:
+            return binarySearch(list, mid + 1, high, value)
         else:
             return mid
 
-    else:
-        return -1
-
-print(binarySearch([1,2,3,4,5,6,7,8,9,10], 0, 9, 3))
+print(binarySearch([1,2,3,4,5,6,7,8,9,10], 0, 9, 8))
